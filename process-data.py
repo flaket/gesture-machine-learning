7dc5f32351764842fee57359c7ee951513a9eea1
Andreas Flakstad <andrfl@stud.ntnu.no>
2015-01-27 17:07:52 +0100

    Sorted data and added own histogram calculation. Initial plotting suggests huge improvements to be made with the results of applying the learning algorithms.

--- process-data.py
+++ process-data.py
@@ -4,9 +4,27 @@ import numpy as np
 from sklearn import preprocessing, svm
 import matplotlib.pyplot as plt
 
-ser = serial.Serial('/dev/ttyUSB1', 9600, timeout=2)
-
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=2)
 data = []
+
+def hist(data, n=128, m=255):
+	'''
+	Partitions an input array of data into an n-sized array with normalized values with regard to m.
+	If the input data is of a shorter length than n the returned array will be sparse.
+	A larger input array will produce a denser result.
+	'''
+	l = len(data)
+	histogram = [0] * n
+	samples = int(l / n)
+	if samples == 0:
+		samples = int(1 / (l / n))
+		for i, x in enumerate(data):
+			histogram[i*samples] = x/m
+	else:
+		for i in range(0,n):
+			histogram[i] = sum(data[i*samples:(i+1)*samples])/(m*samples)
+	return histogram
+
 for i in range(1,2):
 	total = []
 	sensor1 = []
@@ -21,41 +39,34 @@ for i in range(1,2):
 			del line[-1]
 			values = list(map(int, line))
 			total = total + values
-		print(total)
-		for i,x in enumerate(total):
-			if i==0 or (i % 4 == 0):
-				sensor1.append(x)
-			elif i==1 or (i % 3 == 0):
-				sensor2.append(x)
-			elif i==2 or (i % 2 == 0):
-				sensor3.append(x)
-			else: sensor4.append(x)
-		print(sensor1)
-		print(sensor2)
-		print(sensor3)
-		print(sensor4)
-		print(len(total)==len(sensor1)+len(sensor2)+len(sensor3)+len(sensor4))
-		#n, bins = np.histogram(total, bins=96, density=True)
-		#data.append(n)
-		print("Saved data sample: " + str(i))
+		for i in range(0, len(total), 4):
+			sensor1.append(total[i+0])
+			sensor2.append(total[i+1])
+			sensor3.append(total[i+2])
+			sensor4.append(total[i+3])
 	except serial.SerialException:
 		print('caught serial-exception')
+	sensor3.extend(sensor4)
+	sensor2.extend(sensor3)
+	sensor1.extend(sensor2)
+	print(sensor1)
+	#data.append(n)
+	#print(data)
+	result = hist(sensor1)
+	print(result)
+	print(len(result))
+	print(sum(result))
 '''
 with open('s50-left-96data.csv', 'w', newline='') as fp:
     a = csv.writer(fp, delimiter=',')
     a.writerows(data)
 '''
 
-#print(data)
-#n, bins = np.histogram(data, bins=32, density=True)
-#print(data)
+#n, bins = np.histogram(total, bins=96, density=True)
 
-#mu, sigma = 100, 15
-#x = mu + sigma * np.random.randn(10000)
 #width = 0.8 * (bins[1] - bins[0])
 #center = (bins[:-1] + bins[1:]) / 2
-#plt.bar(center, n, align='center', width=width)
-#plt.show()
-
+plt.bar(np.arange(128), tuple(result))
+plt.show()
 
 
