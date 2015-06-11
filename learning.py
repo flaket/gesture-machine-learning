95e8f84e4c3a99fed9ca77025356d335ddab34d8
Andreas A. Flakstad <andflak@gmail.com>
2015-06-11 08:25:35 +0200

    Added confidence level calculation.

--- learning.py
+++ learning.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random as r
+import math
 from sklearn import preprocessing, svm, cross_validation
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, ExtraTreesClassifier, BaggingClassifier
@@ -35,32 +36,40 @@ def create_labels():
     for i in range(samples_per_label):
         y.append("rigth-flick")
 
+def std_dev(lst):
+    n = len(lst)
+    c = sum(lst) / n
+    s = sum((x-c)**2 for x in lst)
+    return math.sqrt(s / (n-1))
+
+def confidence(o):
+    return 1.96 * (o / math.sqrt(loops))
 
 def classify():
     if len(X) == len(y):
         results = []
-        svc_lin_score = 0
-        svc_score = 0
+        svc_lin_score = []
+        svc_score = []
+        logisticRegression_score = []
         kNeighbors_score = 0
         randomForest_score = 0
         gradientBoosting_score = 0
         extraTrees_score = 0
         bagging_score = 0
         decisionTree_score = 0
-        logisticRegression_score = 0
 
         for i in range(loops):
             seed = r.randint(0, len(X))
             X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.25, random_state=seed)
 
             svc_lin = svm.LinearSVC().fit(X_train, y_train)
-            svc_lin_score = svc_lin_score + svc_lin.score(X_test, y_test)
+            svc_lin_score.append(svc_lin.score(X_test, y_test))
 
             svc = svm.SVC(kernel='linear').fit(X_train, y_train)
-            svc_score = svc_score + svc.score(X_test, y_test)
+            svc_score.append(svc.score(X_test, y_test))
 
             logisticRegression = LogisticRegression().fit(X_train, y_train)
-            logisticRegression_score = logisticRegression_score + logisticRegression.score(X_test, y_test)
+            logisticRegression_score.append(logisticRegression.score(X_test, y_test))
 
             '''
             kNeighbors = KNeighborsClassifier().fit(X_train, y_train)
@@ -77,9 +86,21 @@ def classify():
             bagging_score = bagging_score + bagging.score(X_test, y_test)
             '''
 
-        results.append([str(svc_lin_score / loops), "Linear-SVC"])
-        results.append([str(svc_score / loops), "SVC"])
-        results.append([str(logisticRegression_score / loops), "LogisticRegression"])
+        svclin_std_dev = std_dev(svc_lin_score)
+        svclin_avg = sum(svc_lin_score) / loops
+        svclin_conf = confidence(svclin_std_dev)
+
+        svc_std_dev = std_dev(svc_score)
+        svc_avg = sum(svc_score) / loops
+        svc_conf = confidence(svc_std_dev)
+
+        logres_std_dev = std_dev(logisticRegression_score)
+        logres_avg = sum(logisticRegression_score) / loops
+        logres_conf = confidence(logres_std_dev)
+
+        results.append(["Linear-SVC", svclin_avg, svclin_conf])
+        results.append(["SVC", svc_avg, svc_conf])
+        results.append(["LogisticRegression", logres_avg, logres_conf])
         '''
         results.append([str(kNeighbors_score / loops), "KNeighbors"])
         results.append([str(decisionTree_score / loops), "DecisionTree"])
